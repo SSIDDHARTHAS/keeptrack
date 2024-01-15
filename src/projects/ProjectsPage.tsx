e66ff8490ac93e18dd066100628abdf12a475488
SSIDDHARTHAS <siddhartha56rajawat@gmail.com>
2024-01-15 12:40:16 +0530

    Add Functinoality though APIs

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,22 +1,93 @@
-import React, { useState } from "react";
-import { MOCK_PROJECTS } from "./MockProjects";
+import React, { useEffect, useState } from "react";
+// import { MOCK_PROJECTS } from "./MockProjects";
 import ProjectList from "./ProjectList";
 import { Project } from "./Project";
+import { projectAPI } from "./projectAPI";
 
 function ProjectsPage() {
-  const [projects, setProjects] = useState(MOCK_PROJECTS);
+  const [projects, setProjects] = useState<Project[]>([]);
+  const [loading, setLoading] = useState<Boolean>(false);
+  const [error, setError] = useState<string | undefined>(undefined);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  useEffect(() => {
+    setLoading(true);
+    projectAPI
+      .get(currentPage)
+      .then((data) => {
+        setLoading(false);
+        if (currentPage === 1) {
+          setProjects(data);
+        } else {
+          setProjects((projects) => [...projects, ...data]);
+        }
+      })
+      .catch((e) => {
+        setLoading(false);
+        setError(e.message);
+        if (e instanceof Error) {
+          setError(e.message);
+        }
+      });
+  }, [currentPage]);
 
   const saveProject = (project: Project) => {
-    let updatedProjects = projects.map((p: Project) => {
-      return p.id === project.id ? project : p;
-    });
-    setProjects(updatedProjects);
+    projectAPI
+      .put(project)
+      .then((updatedProject) => {
+        let updatedProjects = projects.map((p: Project) => {
+          return p.id === project.id ? new Project(updatedProject) : p;
+        });
+        setProjects(updatedProjects);
+      })
+      .catch((e) => {
+        if (e instanceof Error) {
+          setError(e.message);
+        }
+      });
+  };
+
+  const handleMoreClick = () => {
+    setCurrentPage((currentPage) => currentPage + 1);
   };
 
   return (
     <div>
       <h1>Projects</h1>
+
+      {error && (
+        <div className="row">
+          <div className="card large error">
+            <section>
+              <p>
+                <span className="icon-alert inverse "></span>
+                {error}
+              </p>
+            </section>
+          </div>
+        </div>
+      )}
+
       <ProjectList projects={projects} onSave={saveProject} />
+
+      {!loading && !error && (
+        <div className="row">
+          <div className="col-sm-12">
+            <div className="button-group fluid">
+              <button className="button default" onClick={handleMoreClick}>
+                More...
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {loading === true && (
+        <div className="center-page">
+          <span className="spinner primary"></span>
+          <p>Loading...</p>
+        </div>
+      )}
     </div>
   );
 }
